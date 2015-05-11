344656b3ac109a77caf4663554061cd8ea261bc8
Chris Geyer <chris.w.geyer@gmail.com>
2015-05-11 13:29:25 -0400

    clean html and update alt text for icons

--- src/httpdocs/includes/template_elements/nav.php
+++ src/httpdocs/includes/template_elements/nav.php
@@ -1,7 +1,7 @@
 <ul class="nav nav-pills">
   <li>
     <a href="/Visual-Town-Budget/expenses">
-      <img src="/Visual-Town-Budget/img/money-icon.png" alt='bulb icon'/>
+      <img src="/Visual-Town-Budget/img/money-icon.png" alt='money icon'/>
       Budget Explorer
     </a>
   </li>
@@ -13,127 +13,20 @@
   </li>
   <li>
     <a href="/Visual-Town-Budget/capital">
-      <img src="/Visual-Town-Budget/img/money-icon.png" alt='bulb icon'/>
+      <img src="/Visual-Town-Budget/img/money-icon.png" alt='money icon'/>
       Capital Improvements
     </a>
   </li>
   <li>
     <a href="http://www.raleighnc.gov/government/content/BudgetManagement/Articles/BudgetsAndProcess.html">
-      <img src="/Visual-Town-Budget/img/db-icon.png" alt='bulb icon'/>
+      <img src="/Visual-Town-Budget/img/db-icon.png" alt='process icon'/>
       Budget Process 
     </a>
   </li>
-   <li>
+  <li>
     <a style="" href="https://docs.google.com/forms/d/1oqbJ_RqX8NRNTPsUe3K67VG0BB6Js8yZkYe8fnLnSCk/viewform?usp=send_form" target="_blank">
       <img src="/Visual-Town-Budget/img/feedback-nav.png" alt='feedback icon'/>
       Feedback
     </a>
   </li>
-
-<!-- Old menu with dropdowns
-  <li class="dropdown">
-    <a class="dropdown-toggle" href="whatsnew">
-      <img src="img/bulb-icon.png" alt='bulb icon'/>
-      Tell Me What's New
-      &#8595;
-    </a>
-    <ul class="dropdown-menu">
-      <li>
-        <a href="whatsnew">
-          Understand budget priorities
-        </a>
-      </li>
-      <li>
-        <a href="whatsnew">
-          Highlights of revenue &amp; spending changes
-        </a>
-      </li>      
-      <li>
-        <a href="whatsnew">
-          Interactive budget changes visualization
-        </a>
-      </li>      
-    </ul>
-  </li>
-  <li class="dropdown">
-    <a class="dropdown-toggle" href="expenses">
-      <img src="img/money-icon.png" alt='bulb icon'/>
-      Show me the Money &#8595;
-    </a>
-    <ul class="dropdown-menu">
-      <li>
-        <a href="expenses">
-	  Interactive budget allocation visualization
-        </a>
-      </li>
-      <li>
-        <a href="expenses">
-          See how much goes where
-        </a>
-      </li>
-      <li>
-        <a href="expenses">
-          See changes over time
-        </a>
-      </li>           
-    </ul>
-  </li>
-  <li class="dropdown">
-    <a class="dropdown-toggle" href="resources/resources.php">
-      <img src="img/db-icon.png" alt='bulb icon'/>
-      Resources 
-      &#8595;
-    </a>
-    <ul class="dropdown-menu">
-      <li>
-        <a href="resources/resources.php">
-          Budget Process
-        </a>
-      </li>
-      <li>
-        <a href="resources/resources.php">
-          City Organization
-        </a>
-      </li>            
-      <li>
-        <a href="resources/resources.php">
-          Backup Documentation
-        </a>
-      </li>
-      <li>
-        <a href="resources/resources.php">
-          And much, much more
-        </a>
-      </li>            
-    </ul>
-  </li>
-  <li class="dropdown">
-    <a class="dropdown-toggle" href="about">
-      <img src="img/pencil-icon.png" alt='bulb icon'/>
-      About this Project &#8595;
-    </a>
-     <ul class="dropdown-menu">
-      <li>
-        <a href="about">
-          What's this about?
-        </a>
-      </li>
-      <li>
-        <a href="about">
-          Why are you doing this?
-        </a>
-      </li>
-      <li>
-        <a href="about">
-	   What happens next?
-        </a>
-      </li>
-      <li>
-        <a href="about">
-          How can I participate?
-        </a>
-      </li>                  
-    </ul>
-  </li>      
--->
 </ul>

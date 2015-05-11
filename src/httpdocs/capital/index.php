e4bb645d8d72eb25f6e7ba8c79a3261f4037e4b2
Chris Geyer <chris.w.geyer@gmail.com>
2015-05-11 14:51:01 -0400

    added highlights to capital

--- src/httpdocs/capital/index.php
+++ src/httpdocs/capital/index.php
@@ -12,6 +12,17 @@
       <?php include '../includes/template_elements/top-bar.php'; ?>     
     </div>
     <div>
+    <div class="container wrapper">
+      <div class="well">
+      <h3>Capital Improvements</h3>
+      <ul>
+        <li>The five-year plan includes $10.3 million to maintain and improve fire stations and police facilities</li>
+        <li>Continues long-term funding for street resurfacing, sidewalk repair, traffic lights, and other ongoing infrastructure maintenance</li>
+        <li>Funds maintenance and replacement of technology, including $7 million to replace our aging computer-aid dispatch system</li>
+        <li>The city will invest $142.5 million over the next five years to replace aging water and sewer mains and other infrastructure</li>
+      </ul>
+      </div>
+    </div>
   	  <iframe width="100%" height="600" scrolling="no" src="http://ral.maps.arcgis.com/apps/Embed/index.html?webmap=1cb47fb4459644baaaf7a141b7792c83&amp;extent=-78.7884,35.7223,-78.4293,35.8727&amp;home=true&amp;zoom=true&amp;scale=true&amp;search=true&amp;searchextent=false&amp;legend=true&amp;theme=light"></iframe>
     </div>
     <?php include '../includes/template_elements/footer.php'; ?>

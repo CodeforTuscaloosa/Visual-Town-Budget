277eaedca01c935b8b95f06507f3c32dfeb1bda0
Chris Geyer <chris.w.geyer@gmail.com>
2015-05-11 14:03:48 -0400

    id for highlighted pages

--- src/httpdocs/includes/template_elements/nav.php
+++ src/httpdocs/includes/template_elements/nav.php
@@ -1,18 +1,18 @@
 <ul class="nav nav-pills">
   <li>
-    <a href="/Visual-Town-Budget/expenses">
+    <a href="/Visual-Town-Budget/expenses" id="BE">
       <img src="/Visual-Town-Budget/img/money-icon.png" alt='money icon'/>
       Budget Explorer
     </a>
   </li>
   <li>
-    <a href="/Visual-Town-Budget/whatsnew">
+    <a href="/Visual-Town-Budget/whatsnew" id="WN">
       <img src="/Visual-Town-Budget/img/bulb-icon.png" alt='bulb icon'/>
       New in FY2016 
     </a>
   </li>
   <li>
-    <a href="/Visual-Town-Budget/capital">
+    <a href="/Visual-Town-Budget/capital" id="CI">
       <img src="/Visual-Town-Budget/img/money-icon.png" alt='money icon'/>
       Capital Improvements
     </a>
@@ -24,7 +24,7 @@
     </a>
   </li>
   <li>
-    <a style="" href="https://docs.google.com/forms/d/1oqbJ_RqX8NRNTPsUe3K67VG0BB6Js8yZkYe8fnLnSCk/viewform?usp=send_form" target="_blank">
+    <a href="https://docs.google.com/forms/d/1oqbJ_RqX8NRNTPsUe3K67VG0BB6Js8yZkYe8fnLnSCk/viewform?usp=send_form" target="_blank">
       <img src="/Visual-Town-Budget/img/feedback-nav.png" alt='feedback icon'/>
       Feedback
     </a>

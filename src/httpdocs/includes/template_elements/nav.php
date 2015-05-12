25b64a7373e9f0b699268e4a1f60582c4e4be22d
Chris Geyer <chris.w.geyer@gmail.com>
2015-05-12 10:20:51 -0400

    icon changes in nav

--- src/httpdocs/includes/template_elements/nav.php
+++ src/httpdocs/includes/template_elements/nav.php
@@ -1,32 +1,7 @@
 <ul class="nav nav-pills">
-  <li>
-    <a href="/Visual-Town-Budget/expenses" id="BE">
-      <img src="/Visual-Town-Budget/img/money-icon.png" alt='money icon'/>
-      Budget Explorer
-    </a>
-  </li>
-  <li>
-    <a href="/Visual-Town-Budget/whatsnew" id="WN">
-      <img src="/Visual-Town-Budget/img/bulb-icon.png" alt='bulb icon'/>
-      New in FY2016 
-    </a>
-  </li>
-  <li>
-    <a href="/Visual-Town-Budget/capital" id="CI">
-      <img src="/Visual-Town-Budget/img/money-icon.png" alt='money icon'/>
-      Capital Improvements
-    </a>
-  </li>
-  <li>
-    <a href="http://www.raleighnc.gov/government/content/BudgetManagement/Articles/BudgetsAndProcess.html">
-      <img src="/Visual-Town-Budget/img/db-icon.png" alt='process icon'/>
-      Budget Process 
-    </a>
-  </li>
-  <li>
-    <a href="https://docs.google.com/forms/d/1oqbJ_RqX8NRNTPsUe3K67VG0BB6Js8yZkYe8fnLnSCk/viewform?usp=send_form" target="_blank">
-      <img src="/Visual-Town-Budget/img/feedback-nav.png" alt='feedback icon'/>
-      Feedback
-    </a>
-  </li>
+  <li><a href="/Visual-Town-Budget/expenses" id="BE" class="icons-banknote">Budget Explorer</a></li>
+  <li><a href="/Visual-Town-Budget/whatsnew" id="WN" class="icons-bulb">New in FY2016 </a></li>
+  <li><a href="/Visual-Town-Budget/capital" id="CI" class="icons-location">Capital Improvements</a></li>
+  <li><a href="http://www.raleighnc.gov/government/content/BudgetManagement/Articles/BudgetsAndProcess.html" class="icons-calendar">Budget Process</a></li>
+  <li><a href="" class="icons-bubble">Feedback</a></li>
 </ul>

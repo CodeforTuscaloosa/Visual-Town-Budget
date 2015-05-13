7eb6944fff0ef1acfe00899255f2a2401a4beb05
Chris Geyer <chris.w.geyer@gmail.com>
2015-05-13 13:52:21 -0400

    added read more functionality

--- src/httpdocs/capital/index.php
+++ src/httpdocs/capital/index.php
@@ -5,7 +5,7 @@
     <title>Raleigh, NC Budget Visualization</title>
     <meta name='description' content='Government budgets can be tough to understand, but now the City of Raleigh, North Carolina is providing the next generation of accessibility in financial information that allows citizens to view, engage, and discuss.'>
     <?php include '../includes/template_elements/resources.php'; ?>
-    <link rel="stylesheet" href="flexslider/flexslider.css">
+    <script src="../js/readmore.js"></script>   
   </head>
   <body class="capitalimprovements">
     <div class='header-container'>
@@ -154,6 +154,9 @@
       </div>
   	   <iframe width="100%" height="600" scrolling="no" frameborder="0" src="http://ral.maps.arcgis.com/apps/Embed/index.html?webmap=1cb47fb4459644baaaf7a141b7792c83&amp;extent=-78.7884,35.7223,-78.4293,35.8727&amp;home=true&amp;zoom=true&amp;scale=true&amp;search=true&amp;searchextent=false&amp;legend=true&amp;theme=light"></iframe>
     </div>
+    <script>
+      $('.well').readmore();
+    </script>
     <?php include '../includes/template_elements/footer.php'; ?>
   </body>
 </html>

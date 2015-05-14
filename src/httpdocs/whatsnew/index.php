10ff6b275c7afb562bedd5e3a866d6e3d2206968
adam j martin <adam.ajm@gmail.com>
2015-05-14 17:09:24 -0400

    tweaked heading

--- src/httpdocs/whatsnew/index.php
+++ src/httpdocs/whatsnew/index.php
@@ -123,7 +123,7 @@
     	<div class="row">
         <div class="well">
           <div>
-            <h2>Ten Largest Changes from Last Year's Budget to Proposed FY16 Budget</h2>
+            <h2>Ten Largest Changes in Proposed FY16 Budget over Last Year's Budget</h2>
           </div>
           <div id="mainFlow" class="interactionPanel">
 	          <span id="selectText" class="labelText">Select Fund:</span>

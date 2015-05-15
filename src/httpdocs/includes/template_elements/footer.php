5154c6fed36e1f02b0d4c902ce6c93f6e733daf0
adam j martin <adam.ajm@gmail.com>
2015-05-15 10:06:58 -0400

    removed twitter code

--- src/httpdocs/includes/template_elements/footer.php
+++ src/httpdocs/includes/template_elements/footer.php
@@ -26,14 +26,6 @@
           <p>
            Your data. Your Raleigh.<br>Making civic engagement easier.
           </p>
-          <p>
-            <a class="twitter-follow-button"
-              href="https://twitter.com/openraleigh"
-              data-show-count="false"
-              data-size="large">
-              Follow @OpenRaleigh
-              <script>window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));</script>
-          </p>
         </div>
        <!-- FOOTER COLUMN 3 -->
         <div class='span4 omega feedback'>

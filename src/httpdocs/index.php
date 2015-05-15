4568a6cff8aa42b629da47c389dc394b31e2f9ab
Chris Geyer <chris.w.geyer@gmail.com>
2015-05-15 12:34:53 -0400

    updated GA script

--- src/httpdocs/index.php
+++ src/httpdocs/index.php
@@ -32,13 +32,13 @@
       require_once 'includes/datafiles.php';
     ?>
     <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-10273473-12', 'Ashevillevisualbudget.org');
-      ga('send', 'pageview');
+    ga('create', 'UA-9880547-7', 'auto');
+    ga('send', 'pageview');
     </script>
     <script> 
     var $buoop = {c:2}; 

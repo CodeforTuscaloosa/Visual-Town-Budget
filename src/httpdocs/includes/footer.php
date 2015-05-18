b6551b22815f41f7b4321d9afc2b301a0ffcacf8
adam j martin <adam.ajm@gmail.com>
2015-05-18 10:19:13 -0400

    footer updates

--- src/httpdocs/includes/footer.php
+++ src/httpdocs/includes/footer.php
@@ -1,5 +1,8 @@
  <div id="footer" style="font-size:1.2em">
+  *Note: 2016 data reflects the Proposed Budget, not the final Adopted budget. Council has until June 30 to adopt a final budget. 
+  <br> FY refers to the Fiscal Year, which spans July 2015 to June 2016.
+  <br>
   *Source: <a class="link" href="https://data.raleighnc.gov/category/finance">Open Data</a> 
   from the 
-  <a class="link" target="_blank" href="http://www.raleighnc.gov/">City of Raleigh</a>
+  <a class="link" target="_blank" href="http://www.raleighnc.gov/">City of Raleigh</a>. 
 </div>

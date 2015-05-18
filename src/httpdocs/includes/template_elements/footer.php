e7b85738874ee7952ba8d2efd6caa5a26418a025
Chris Geyer <chris.w.geyer@gmail.com>
2015-05-18 10:08:32 -0400

    spacing

--- src/httpdocs/includes/template_elements/footer.php
+++ src/httpdocs/includes/template_elements/footer.php
@@ -5,20 +5,10 @@
         <!-- FOOTER COLUMN 1 -->
         <div class='span4 alpha collaboration'>
           <img src="http://www.raleighnc.gov/content/static/images/City-Seal-BW.png" alt="City of Raleigh Seal" />
-          <p>
-            a collaborative web effort <br> brought to you by:
-          </p>                  
-          <a href="http://www.raleighnc.gov" target="_blank">
-            The City of Raleigh
-          </a>
-          <span>, </span>
-          <a href="http://www.codeforraleigh.com" target="_blank">
-            Code for Raleigh
-          </a>
-           <span>&amp;</span>
-           <a href="http://www.codeforasheville.org/" target="_blank">
-            Code for Asheville
-          </a>
+          <p>a collaborative web effort <br> brought to you by:</p>                  
+          <a href="http://www.raleighnc.gov" target="_blank">The City of Raleigh</a><span>, </span>
+          <a href="http://www.codeforraleigh.com" target="_blank">Code for Raleigh</a><span> &amp; </span>
+           <a href="http://www.codeforasheville.org/" target="_blank">Code for Asheville</a>
         </div>
         <!-- FOOTER COLUMN 2 --> 
         <div class='span4 alpha collaboration'>

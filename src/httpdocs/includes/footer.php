afeb408c8df893ab9c520a1f5fac9cfcf7680b92
adam j martin <adam.ajm@gmail.com>
2015-05-18 16:42:59 -0400

    footer update

--- src/httpdocs/includes/footer.php
+++ src/httpdocs/includes/footer.php
@@ -1,7 +1,5 @@
  <div id="footer" style="font-size:1.2em">
-  Note: The revenue and expense amounts shown above include only operating funds. 
-  For example, for FY 2016, $1,020 million refers only to the total proposed operating expenses. 
-  If you add the $215.6 million in capital funds budgeted for FY2016 (described separately <a class="link" href="/budgetvisualization/capital">here</a>), you will arrive at the total FY2016 budget $1,236 million described in the budget report.
+  Note: The revenue and expense amounts shown above include only operating funds. Additional capital budget information is available <a class="link" href="/budgetvisualization/capital">here</a>).
   <br> FY refers to the City's Fiscal Year, which spans July 1 to June 30.
   <br>
   *Source: <a class="link" href="https://data.raleighnc.gov/category/finance">Open Data</a> 

346e6cf67719afae58dff84f5726e83ec36a3734
Chris Geyer <chris.w.geyer@gmail.com>
2015-05-19 10:00:05 -0400

    widths for nav so title doesn't wrap

--- src/httpdocs/includes/template_elements/top-bar.php
+++ src/httpdocs/includes/template_elements/top-bar.php
@@ -1,14 +1,14 @@
 <div class="header">
   <div class='container'>
     <div class="row header-area">   
-      <div class="span3">
+      <div class="span4">
         <h1>
           <a href="/budgetvisualization/expenses">
             <span class="raleigh">City of Raleigh <br/> Budget Visualization</span>
           </a>
         </h1>
       </div>
-      <div class="span9">
+      <div class="span8">
         <div class="navigation">
           <?php include 'nav.php'; ?>
         </div>

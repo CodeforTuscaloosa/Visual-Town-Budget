be0669dd804efc38540a804fde24ad02f08248b1
Chris Geyer <chris.w.geyer@gmail.com>
2015-05-20 11:16:32 -0400

    added meta tags

--- src/httpdocs/capital/index.php
+++ src/httpdocs/capital/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <title>Raleigh, NC Budget Visualization</title>
     <meta name='description' content='Government budgets can be tough to understand, but now the City of Raleigh, North Carolina is providing the next generation of accessibility in financial information that allows citizens to view, engage, and discuss.'>

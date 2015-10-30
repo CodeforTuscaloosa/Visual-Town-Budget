bbf2f84bb19dffa06202153ef5f4cefefd6dfede
Matthew Leeds <mwl458@gmail.com>
2015-10-30 13:35:08 -0500

    Start the transition from PHP to Jekyll. The data viz container is broken atm.

--- js/avb.js
+++ js/avb.js
@@ -208,7 +208,7 @@ window.onpopstate = popUrl;
 function pushUrl(section, year, mode, node) {
     if (ie()) return;
     // format URL
-    var url = '/budgetvisualization/' + section + '/' + year + '/' + mode + '/' + node;
+    var url = '/' + section + '/' + year + '/' + mode + '/' + node;
     // create history object
     window.history.pushState({
         section: section,
--- src/httpdocs/data/old_data/README.txt
+++
@@ -1,1 +0,0 @@
-unused data files. filed for reference use

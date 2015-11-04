287945a9fd466a479d89ef2ddf7a18453cfdb000
Matthew Leeds <mwl458@gmail.com>
2015-11-03 18:26:31 -0600

    Fix a bug where the wrong columns were being deleted during comma cleanup.

--- processing/fix_commas.py
+++ processing/fix_commas.py
@@ -4,9 +4,9 @@
 # Unfortunately, the University of Alabama expense data from open.ua.edu 
 # does not conform to the CSV specification and quote fields that contain
 # commas, so this script attempts to fix that by assuming the Agency 
-# column is only ever one of the hardcoded values below, and joining
-# the 3rd and 4th columns otherwise (usually it's the payee's name
-# that has a comma. This script should only be run on the unaltered
+# column is only ever one of the hardcoded values below, and otherwise
+# combining the columns between the payee (which usually has an extra comma)
+# and the category. This script should only be run on the unaltered
 # "dirty" data; its output is not suitable as input.
 #
 
@@ -29,12 +29,13 @@ with open('../_data/ua_expenses.csv') as inFile:
                 for agency in VALID_AGENCIES:
                     try:
                         agency_column = row_copy.index(agency)
+                        break
                     except ValueError:
                         pass
                 if agency_column == -1: continue
                 row_copy[2] = ','.join(row_copy[2:agency_column - 1])
-                for i in range(3,agency_column - 1):
-                    row_copy.pop(i)
+                for i in range(3, agency_column - 1):
+                    row_copy.pop(3)
             if len(row_copy) > 2:
                 row_copy.pop(0)
                 row_copy.pop(len(row_copy) - 1) # delete empty column
